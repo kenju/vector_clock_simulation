46de481b18b6e5e97c7896b887c4990c6b1deb18
kenju <ken901waga@gmail.com>
2022-05-31 11:15:46 +0100

    add new example

--- src/lamport_timestamps_test.py
+++ src/lamport_timestamps_test.py
@@ -160,6 +160,79 @@ class LamportTimestampTest(unittest.TestCase):
         self.assertListEqual(processed[p2], [2, 3, 4, 5, 6, 7, 10, 11, 12])
         self.assertListEqual(processed[p3], [1, 4, 7, 8, 12, 13])
 
+    def test_process_exampleC(self):
+        # initialize messages
+        m1 = Message(1)
+        m2 = Message(2)
+        m3 = Message(3)
+        m4 = Message(4)
+        m5 = Message(5)
+        m6 = Message(6)
+        m7 = Message(7)
+        m8 = Message(8)
+        m9 = Message(9)
+        m10 = Message(10)
+        m11 = Message(11)
+
+        # initialize events
+        p1 = Process(
+            process_id=1,
+            events=[
+                EventMessageSend(m1),
+                EventMessageSend(m2),
+                EventMessageSend(m3),
+                EventMessageRecv(m7),
+                EventMessageSend(m4),
+                EventMessageSend(m5),
+            ],
+        )
+        p2 = Process(
+            process_id=2,
+            events=[
+                EventMessageRecv(m8),
+                EventMessageSend(m6),
+                EventMessageSend(m7),
+                EventMessageRecv(m10),
+                EventMessageRecv(m2),
+            ],
+        )
+        p3 = Process(
+            process_id=3,
+            events=[
+                EventMessageRecv(m9),
+                EventMessageSend(m8),
+                EventMessageRecv(m1),
+                EventMessageRecv(m4),
+                EventMessageRecv(m6),
+                EventMessageRecv(m11),
+                EventMessageRecv(m5),
+            ],
+        )
+        p4 = Process(
+            process_id=4,
+            events=[
+                EventMessageSend(m9),
+                EventMessageSend(m10),
+                EventMessageRecv(m3),
+                EventMessageSend(m11),
+            ],
+        )
+
+        logger = Logger(LogLevel.DEBUG)
+        logger.set_logfile("log/test_lamport_exampleC.log")
+        processes = [p1, p2, p3, p4]
+        lamport = LamportTimestampSolver(logger, processes)
+
+        lamport.print()
+
+        self.assertEqual(lamport.total_event_count(), 22)
+
+        processed = lamport.process()
+        self.assertListEqual(processed[p1], [1, 2, 3, 7, 8, 9])
+        self.assertListEqual(processed[p2], [4, 5, 6, 7, 8])
+        self.assertListEqual(processed[p3], [2, 3, 4, 9, 10, 11, 12])
+        self.assertListEqual(processed[p4], [1, 2, 4, 5])
+
 
 if __name__ == '__main__':
     unittest.main()
--- src/vectorclock_test.py
+++ src/vectorclock_test.py
@@ -240,5 +240,126 @@ class VectorClockTimestampTest(unittest.TestCase):
             [1, 0, 5, 4],
         ])
 
+
+    def test_process_exampleC(self):
+        # initialize messages
+        m1 = Message(1)
+        m2 = Message(2)
+        m3 = Message(3)
+        m4 = Message(4)
+        m5 = Message(5)
+        m6 = Message(6)
+        m7 = Message(7)
+        m8 = Message(8)
+        m9 = Message(9)
+        m10 = Message(10)
+        m11 = Message(11)
+
+        # initialize events
+        p0 = Process(
+            process_id=0,
+            events=[
+                EventMessageSend(m1),
+                EventMessageSend(m2),
+                EventMessageSend(m3),
+                EventMessageRecv(m7),
+                EventMessageSend(m4),
+                EventMessageSend(m5),
+            ],
+        )
+        p1 = Process(
+            process_id=1,
+            events=[
+                EventMessageRecv(m8),
+                EventMessageSend(m6),
+                EventMessageSend(m7),
+                EventMessageRecv(m10),
+                EventMessageRecv(m2),
+            ],
+        )
+        p2 = Process(
+            process_id=2,
+            events=[
+                EventMessageRecv(m9),
+                EventMessageSend(m8),
+                EventMessageRecv(m1),
+                EventMessageRecv(m4),
+                EventMessageRecv(m6),
+                EventMessageRecv(m11),
+                EventMessageRecv(m5),
+            ],
+        )
+        p3 = Process(
+            process_id=3,
+            events=[
+                EventMessageSend(m9),
+                EventMessageSend(m10),
+                EventMessageRecv(m3),
+                EventMessageSend(m11),
+            ],
+        )
+
+        logger = Logger(LogLevel.DEBUG)
+        logger.set_logfile("log/test_vectorclock_exampleC.log")
+        processes = [p0, p1, p2, p3]
+        vectorclock = VectorClockTimestampSolver(logger, processes)
+
+        vectorclock.print()
+
+        self.assertEqual(vectorclock.total_event_count(), 22)
+
+        processed = vectorclock.process()
+        self.assertListEqual(processed[p0], [
+            [1, 0, 0, 0],
+            [2, 0, 0, 0],
+            [3, 0, 0, 0],
+            [4, 3, 2, 1],
+            [5, 3, 2, 1],
+            [6, 3, 2, 1],
+        ])
+        self.assertListEqual(processed[p1], [
+            [0, 1, 2, 1],
+            [0, 2, 2, 1],
+            [0, 3, 2, 1],
+            [0, 4, 2, 2],
+            [2, 5, 2, 2],
+        ])
+        self.assertListEqual(processed[p2], [
+            [0, 0, 1, 1],
+            [0, 0, 2, 1],
+            [1, 0, 3, 1],
+            [5, 3, 4, 1],
+            [5, 3, 5, 1],
+            [5, 3, 6, 4],
+            [6, 3, 7, 4],
+        ])
+        self.assertListEqual(processed[p3], [
+            [0, 0, 0, 1],
+            [0, 0, 0, 2],
+            [3, 0, 0, 3],
+            [3, 0, 0, 4],
+        ])
+
+        concurrent_events = vectorclock.concurrent_events(
+            another=[0, 0, 0, 2],
+        )
+        self.assertListEqual(concurrent_events, [
+            [1, 0, 0, 0],
+            [2, 0, 0, 0],
+            [3, 0, 0, 0],
+            [4, 3, 2, 1],
+            [5, 3, 2, 1],
+            [6, 3, 2, 1],
+            [0, 1, 2, 1],
+            [0, 2, 2, 1],
+            [0, 3, 2, 1],
+            [0, 0, 1, 1],
+            [0, 0, 2, 1],
+            [1, 0, 3, 1],
+            [5, 3, 4, 1],
+            [5, 3, 5, 1],
+        ])
+
+
 if __name__ == '__main__':
     unittest.main()
